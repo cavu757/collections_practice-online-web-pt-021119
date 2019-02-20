c1767c038aac983fea1f378955b4949509074406
Jose Pehovaz-Diez <jose.pehovazdiez@gmail.com>
2019-02-20 04:43:48 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -9,3 +9,28 @@ end
 def sort_array_char_count(array)
   array.sort 
 end
+
+def swap_elements
+
+end
+
+def reverse_array
+
+end
+
+def kesha_maker
+
+end
+
+def find_a
+  
+end
+
+def sum_array
+  
+end
+
+def add_s
+  
+end
+
